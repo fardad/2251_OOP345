cc3a69b54c70278e28569bce9b78c2a0c688d5f3
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2025-01-17 17:07:48 -0500

    Rule Of Five

--- NAA/04-Jan17/prg.cpp
+++ NAA/04-Jan17/prg.cpp
@@ -10,12 +10,37 @@ public:
          Cstr::alocpy( m_value, value );
       }
    }
+   Name( const Name& N ) {
+      if ( N.m_value ) {
+         Cstr::alocpy( m_value, N.m_value );
+      }
+   }
    Name& operator=( const Name& N ) {
       if ( this != &N ) {
          Cstr::alocpy( m_value, N.m_value );
       }
       return *this;
    }
+   Name( Name&& N ) { // move constructor
+      if ( N.m_value ) {
+         m_value = N.m_value;
+         N.m_value = nullptr;
+      }
+   }
+   //Name( Name&& N ) { // move constructor
+   //   operator=( move(N) );
+   //}
+
+   Name& operator=( Name&& N ) { // move assignment
+      if ( this != &N ) {
+         delete[] m_value;
+         m_value = N.m_value;
+         N.m_value = nullptr;
+      }
+      return *this;
+   }
+
+
    ostream& print( ostream& os )const {
       os << (m_value ? m_value : "");
       return os;
@@ -27,19 +52,32 @@ public:
 ostream& operator<< ( ostream& os, const Name& N ) {
    return N.print( os );
 }
+void WhichValue( int& lv ) {
+   cout << "lv: " << lv << endl;
+}
+
+void WhichValue( int&& rv ) {
+   cout << "rv: " << rv << endl;
+}
+
+void WhichValue( Name& lv ) {
+   cout << "lv: " << lv << endl;
+}
+void WhichValue( Name&& rv ) {
+   cout << "rv: " << rv << endl;
+}
+
+
 int main( ) {
-   Name F = "Fred", G, H;
-   int a;
+   int i = 10;
+   Name N = "Fred";
+   Name F = "Jack";
 
-   a = 10;
+   cout << "N: " << N << " F:" << F << endl;
 
-   G = F;
+   N = move( F );
 
-   F = Name( "Jonh" );
-
-   Name( "John" ) = G;
-   
-   H = "Henry";
+   cout << "N: " << N << " F:" << F << endl;
 
    return 0;
 }
